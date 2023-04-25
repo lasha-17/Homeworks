302cd89db69ec4bb5abfb727879812d629865e9d
lasha-17 <126957934+lasha-17@users.noreply.github.com>
2023-04-25 17:08:31 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let firstName = "Lasha";
+let lastName = "Makhniashvili";
+let birthDate = 1998;
+let gender = "male";
+
+
+let country = "Georgia";
+let population = 3.7;
+
+
+console.log(firstName + "" + lastName + "" + birthDate + "" + gender);
+
+console.log(country + "" + population + "" + 'million');
