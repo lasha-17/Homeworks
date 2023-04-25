0398fb2a2db3897aed42d6842626e801a0fea9db
lasha-17 <126957934+lasha-17@users.noreply.github.com>
2023-04-25 17:24:19 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let firstName = "Lasha";
 let lastName = "Makhniashvili";
-let birthDate = 1998;
+let birthYear = 1998;
 let gender = "male";
 
 
@@ -8,6 +8,6 @@ let country = "Georgia";
 let population = 3.7;
 
 
-console.log(firstName + "" + lastName + "" + birthDate + "" + gender);
+console.log(firstName + "" + lastName + "" + birthYear + "" + gender);
 
-console.log(country + "" + population + "" + 'million');
+console.log(country + "'s population is" + population + "" + 'million');
