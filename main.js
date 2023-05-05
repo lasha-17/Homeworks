60dd56622aa516a39dde4faae400e03b82c47096
lasha-17 <126957934+lasha-17@users.noreply.github.com>
2023-05-06 03:08:30 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,56 @@
-let firstName = "Lasha";
-let lastName = "Makhniashvili";
-let birthYear = 1998;
-let gender = "male";
+//problem/1
+
+let y = 5;
+console.log(5+2);
+console.log(7-6);
+console.log(1*30);
+console.log(30/2);
+console.log(15%5);
+console.log(0**3);
+
+//problem/2
+
+let x = 5;
+console.log(x);
+
+x += 2;
+x -= 7;
+x *= 30;
+x /= 2;
+x %= 5;
+x **= 3;
+console.log(x);
+
+//problem/3
 
 
-let country = "Georgia";
-let population = 3.7;
+let a = 0.1 + 0.2;
+console.log(a);
+let b = 0.3;
+console.log(a === b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
 
 
-console.log(firstName + "" + lastName + "" + birthYear + "" + gender);
+//problems/4
 
-console.log(country + "'s population is" + population + "" + 'million');
+
+
+let age = 18; 
+let firstName = "James";
+let price = 35;
+
+if (age >17) { 
+    price =0;
+    console.log("priceBeFree");
+}  
+if (age ==25 && firstName === "James") {
+    price =28
+    console.log("discount20procents");
+} else (age <18)
+ {  price =35;
+    console.log("anyOtherCasePriceShouldStayTheSame");
+} 
